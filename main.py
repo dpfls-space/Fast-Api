bcbc3bda2c12380f9d07e34da0da7ff0fa3e2343
dpfls-space <38265ye@gmail.com>
2026-07-08 21:21:12 +0900

    Feat: users 엔드포인트 fastapi 생성 및 라우터 등록 - create_user() 는 추후 구현

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 import uvicorn
 
 from fastapi import FastAPI
+from user.interface.controllers.user_controller import router as user_routers
 
 app = FastAPI()
-
-@app.get("/")
-def hello():
-    return { "Hello": "FastAPI" }
-
+app.include_router(user_routers)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", reload=True)
