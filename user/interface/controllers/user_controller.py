275bb485e9ad15d954df091facd608d4651fdd0f
dpfls-space <38265ye@gmail.com>
2026-07-08 22:02:34 +0900

    Feat: 유저 생성 API에 요청 바디 검증 모델 추가

--- user/interface/controllers/user_controller.py
+++ user/interface/controllers/user_controller.py
@@ -1,7 +1,13 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
 
 router = APIRouter(prefix="/users")
 
+class CreateUserBody(BaseModel):
+    name: str
+    email: str
+    password: str
+
 @router.post("", status_code=201)
-def create_user():
-    return "user created"
+def create_user(user: CreateUserBody):
+    return user
